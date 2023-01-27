67fab6981848cd0cc6d97ca31b9f060821c97f94
Zixuan Chen <remch183@outlook.com>
2023-01-27 23:49:19 +0800

    fix: add top level await

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), wasm()],
+  plugins: [vue(), wasm(), topLevelAwait()],
 });
